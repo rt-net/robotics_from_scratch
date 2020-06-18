4d4dd803296dab2cc61b79b16800dd6bb2a8b8bf
kenjiinukai <36922159+kenjiinukai@users.noreply.github.com>
2020-06-18 15:26:49 +0900

    Change the lines of license and copyright

--- ch01/main.c
+++ ch01/main.c
@@ -1,17 +1,3 @@
-// Copyright 2020 RT Corporation
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /**
  * @file main.c
  * @brief servo test
@@ -82,3 +68,18 @@ int main()
   closeCranex7Port();  //シリアルポートを閉じる
   return 0;
 }
+
+// Copyright 2020 RT Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// To make line numbers be identical, this statement is written here. Don't move it to the header.
