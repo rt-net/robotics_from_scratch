b2edd5e7eabe2f844b9ebab217fa19dabdd0b9a6
kenjiinukai <36922159+kenjiinukai@users.noreply.github.com>
2020-06-18 15:31:46 +0900

    Change the lines of license and copyright.

--- common/matrix.h
+++ common/matrix.h
@@ -1,3 +1,9 @@
+/**
+ * @file matrix.h
+ * @brief bare minimum matrix library
+ * @author RT Corporation
+ * @date 2019/03/22
+ */
 // Copyright 2019 RT Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,13 +17,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-/**
- * @file matrix.h
- * @brief bare minimum matrix library
- * @author RT Corporation
- * @date 2019/03/22
- */
 
 #ifndef MATRIX_H_
 #define MATRIX_H_
