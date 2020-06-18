27143cb38d4fa6e13db653fb34d7811c1f208dd8
kenjiinukai <36922159+kenjiinukai@users.noreply.github.com>
2020-06-18 15:30:20 +0900

    Change the lines of license and copyright

--- common/crane_x7_comm.h
+++ common/crane_x7_comm.h
@@ -1,3 +1,9 @@
+/**
+ * @file crane_x7_control.h
+ * @brief Wrapper functions of DynamixelSDK
+ * @author RT Corporation
+ * @date 2020/03/21
+ */
 // Copyright 2020 RT Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,13 +17,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-/**
- * @file crane_x7_control.h
- * @brief Wrapper functions of DynamixelSDK
- * @author RT Corporation
- * @date 2020/03/21
- */
 
 #ifndef CRANE_X7_CONTROL_H_
 #define CRANE_X7_CONTROL_H_
